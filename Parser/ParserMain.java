2f44c85b3e646d79154e4a6d20c175e403384394
Samantha <samantharhay@tamu.edu>
2018-09-27 18:37:29 -0500

    finished it up
    changed somethings around renamed some variables, cited some things, and commented changes

--- Parser/ParserMain.java
+++ Parser/ParserMain.java
@@ -10,13 +10,14 @@ import org.antlr.v4.runtime.tree.*;
 public class ParserMain {
 		public static Boolean isValid(String testString) {
 				Boolean valid = true;
+				int count = 1;
 				//If no exception is thrown, the input is valid...
 				try{
 					//Check the input against the lexer and parser grammar
 					//Help from https://www.baeldung.com/java-antlr
 					ANTLRInputStream input = new ANTLRInputStream(testString);
 					ParserLexer lexer = new ParserLexer(input);
-					lexer.removeErrorListeners();
+					//lexer.removeErrorListeners();
 					CommonTokenStream tokens = new CommonTokenStream(lexer);
 					ParserParser parser = new ParserParser(tokens);
 					//Allow the input to throw an error if it doesn't match the grammar.
@@ -29,6 +30,7 @@ public class ParserMain {
 							}
 						}
 					);
+					
 					//Do not print that there was an error
 					parser.removeErrorListeners();
 					//Create a tree.
@@ -36,25 +38,39 @@ public class ParserMain {
 					ParseTreeWalker walker = new ParseTreeWalker();
 					ParserBaseListener listener = new ParserBaseListener();
 					walker.walk(listener, tree);
+					count = parser.getNumberOfSyntaxErrors();
 				}
 				//...otherwise, the input is invalid.
 				catch (Exception e) {
 					valid = false;
 				}
-				return valid;
+				//double checks for errors
+				if(count != 0){
+					valid = false;
+				}
+				//makes sure there is a ; and that there is only 1
+				if(testString.substring(testString.length()-1).equals(";") && (!(testString.substring(testString.length()-2,testString.length()-1).equals(";")))){
+					return valid;
+				}
+				else{
+					valid = false;
+					return valid;
+				}
 		}
 		
 		public static void main(String[] args) {
 			Scanner keyboard = new Scanner(System.in);
-			BufferedWriter bufferout = null;
+			//Help from https://beginnersbook.com/2014/01/how-to-write-to-file-in-java-using-bufferedwriter/ with writing to a file
+			BufferedWriter bufferOut = null;
 			try{
 				int lineNumber = 1;
-				File outfile = new File("output.txt");
-				if(!outfile.exists()){
-					outfile.createNewFile();
+				File outFile = new File("output.txt"); //output will write to file output.txt
+				//checks it there is already a file by this name and if not makes one
+				if(!outFile.exists()){
+					outFile.createNewFile();
 				}
-				FileWriter outwriter = new FileWriter(outfile);
-				bufferout = new BufferedWriter(outwriter);
+				FileWriter outWriter = new FileWriter(outFile);
+				bufferOut = new BufferedWriter(outWriter);
 				//Use isValid() to check the validity of each nonempty line in the input, and then print the results
 				while (keyboard.hasNextLine()) {
 					if(keyboard.hasNext()){
@@ -66,31 +82,22 @@ public class ParserMain {
 							//Make sure the line ends in a semicolon
 							String lastChar = inputLine.substring(inputLine.length() - 1);
 							//Print appropriate result
+							//program writes to both the window and output.txt file
 							if (valid == true) {
-								if (lastChar.equals(";")) {
 									System.out.print("Line ");
-									bufferout.write("Line ");
+									bufferOut.write("Line ");
 									System.out.print(lineNumber);
-									bufferout.write(lineNumber+"");
+									bufferOut.write(lineNumber+"");
 									System.out.println(" is valid");
-									bufferout.write(" is valid\n");
-								}
-								else if (!lastChar.equals(";")) {
-									System.out.print("Line ");
-									bufferout.write("Line ");
-									System.out.print(lineNumber);
-									bufferout.write(lineNumber+"");
-									System.out.println(" is invalid");
-									bufferout.write(" is invalid\n");
-								}
+									bufferOut.write(" is valid\n");
 							}
 							else if (valid == false) {
 								System.out.print("Line ");
-								bufferout.write("Line ");
+								bufferOut.write("Line ");
 								System.out.print(lineNumber);
-								bufferout.write(lineNumber+"");
+								bufferOut.write(lineNumber+"");
 								System.out.println(" is invalid");
-								bufferout.write(" is invalid\n");
+								bufferOut.write(" is invalid\n");
 							}
 							//Move to the next line
 							lineNumber = lineNumber + 1;
@@ -104,8 +111,9 @@ public class ParserMain {
 			finally
 			{
 				try{
-					if(bufferout!=null)
-						bufferout.close();
+					//closes buffer
+					if(bufferOut!=null)
+						bufferOut.close();
 				}
 				catch(Exception ex){
 					System.out.println("error");
