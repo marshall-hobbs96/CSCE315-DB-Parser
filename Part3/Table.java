f4427d13efca70c38d813dbbf72944ad7d08a5d5
Samantha <samantharhay@tamu.edu>
2018-10-02 22:05:13 -0500

    some improvements

--- Part3/Table.java
+++ Part3/Table.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 
 public class Table{
-	public String tital;
+	public String title;
 	public ArrayList<ArrayList<String>> datas;
 	
 	//d needs to be a vector of vectors, [0] is name of vector,
 	//[1] is the char limit, null when int
 	public Table(String name,ArrayList<ArrayList<String>> d){ //d needs to be a vector of vectors
-		tital = name;
+		title = name;
 		datas=d;
 	}
 	
@@ -19,8 +19,35 @@ public class Table{
 	}
 
 	public void tablePrint(){
-		System.out.println(tital);
+		System.out.println(title);
 		System.out.println(datas);
+	}
+	
+	//VERY IMPORTANT: if you are removing multiple elements from the list delete from bottom up so 6 to 0 not 0 to 6
+	public void deleteElement(int index){
+		if(index<datas.get(0).size() && index>1){
+			for(int i = 0; i<datas.size();i++){
+				datas.get(i).remove(index);
+			}
+		}
+		else{
+			System.out.println("Error: tried to remove value out of index");
+		}
+	}
+	
+	public ArrayList<Integer> searchColumn(String colHead, String searchFor){
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
+		for(int i = 0; i<datas.size();i++){
+			if(datas.get(i).get(0).equals(colHead)){
+				for(int j = 2; j<datas.get(i).size();j++){
+					if(datas.get(i).get(j).equals(searchFor)){
+						indexes.add(j);
+					}
+				}
+				return indexes;
+			}
+		}
+		return indexes;
 	}
 	
 	public static void main(String[] args) {
@@ -39,6 +66,14 @@ public class Table{
 		first.add("fido");
 		first.add("poodle");
 		test.addElement(first);
+		ArrayList<String> second = new ArrayList<String>();
+		second.add("Rex");
+		second.add("German Sheperd");
+		test.addElement(second);
+		test.addElement(second);
+		//test.deleteElement(3);
+		System.out.println(test.searchColumn("name","ex"));
 		test.tablePrint();
+		
 	}
 }
